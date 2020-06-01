1638af82e9647ea512b79ca79d59196572f8470d
Mikołaj Antczak <antczakmikolaj19@gmail.com>
2020-06-01 10:50:31 +0200

    Added not working Texture Manager and pointers (need fix)
    There is a problem with getGlobalBounds metod it has to do something with that i made platforms a unique pointers

---
+++ EPG/include/Platforms.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class Platforms : public sf::Sprite
+{
+    public:
+        Platforms();
+        virtual ~Platforms();
+
+    protected:
+
+    private:
+};
+
+
--- EPG/main.cpp
+++ EPG/main.cpp
@@ -2,57 +2,67 @@
 #include <SFML/Graphics.hpp>
 
 #include <iostream>
+#include <memory>
+#include <vector>
 
 #include "Player.h"
+#include "TextureManager.h"
+#include "Platforms.h"
+
+sf::Texture loadTexture(std::string path_to_file)
+{
+    sf::Texture texture;
+    if (!texture.loadFromFile(path_to_file)) {
+        std::cerr << "Could not load texture" << std::endl;
+    }
+    return texture;
+}
+
+void create_platforms(std::vector<sf::Sprite> &platforms)
+{
+    auto wall1= std::make_unique<sf::Sprite>();
+    wall1->setTexture(*TextureManager::getTexture("wall"));
+    wall1->setPosition(0, 400);
+    platforms.emplace_back(std::move(wall1));
+
+    auto wall2= std::make_unique<sf::Sprite>();
+    wall2->setTexture(*TextureManager::getTexture("wall"));
+    wall2->setPosition(200, 400);
+    platforms.emplace_back(std::move(wall2));
+
+    auto wall3= std::make_unique<sf::Sprite>();
+    wall3->setTexture(*TextureManager::getTexture("wall"));
+    wall3->setPosition(400, 400);
+    platforms.emplace_back(std::move(wall3));
+
+    auto wall4= std::make_unique<sf::Sprite>();
+    wall4->setTexture(*TextureManager::getTexture("wall"));
+    wall4->setPosition(600, 400);
+    platforms.emplace_back(std::move(wall4));
+}
+
+
 
 int main() {
     // Load textures
-    sf::Texture texture_guy;
-    if (!texture_guy.loadFromFile("textures/guy.png")) {
-        std::cerr << "Could not load texture" << std::endl;
-        return 1;
-    }
-
-    sf::Texture texture_wall;
-    if (!texture_wall.loadFromFile("textures/wall.png")) {
-        std::cerr << "Could not load texture" << std::endl;
-        return 1;
-    }
-
+    TextureManager::loadTexture("guy", "textures/guy.png");
+    TextureManager::loadTexture("wall", "textures/wall.png");
 
     // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-
-    // create some shapes
-    sf::CircleShape circle(100.0);
-    circle.setPosition(100.0, 300.0);
-    circle.setFillColor(sf::Color(100, 250, 50));
-
-    sf::RectangleShape rectangle(sf::Vector2f(120.0, 60.0));
-    rectangle.setPosition(500.0, 400.0);
-    rectangle.setFillColor(sf::Color(100, 50, 250));
-
-    sf::ConvexShape triangle;
-    triangle.setPointCount(3);
-    triangle.setPoint(0, sf::Vector2f(0.0, 0.0));
-    triangle.setPoint(1, sf::Vector2f(0.0, 100.0));
-    triangle.setPoint(2, sf::Vector2f(140.0, 40.0));
-    triangle.setOutlineColor(sf::Color::Red);
-    triangle.setOutlineThickness(5);
-    triangle.setPosition(600.0, 100.0);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Eeasy Platform Game");
 
     // Create player
     Player player;
-    player.setTexture(texture_guy);
+    player.setTexture(*TextureManager::getTexture("guy"));
 
-    // Create wall
-    sf::Sprite wall;
-    wall.setTexture(texture_wall);
-    wall.setPosition(300, 500);
-    //wall.setScale(0.5, 0.1);
+    // Create platforms
+    std::vector<sf::Sprite> platforms;
+    create_platforms(platforms);
 
+    // Create clock
     sf::Clock clock;
 
+    // Set frame rate limit
     window.setFramerateLimit(60);
 
 
@@ -70,23 +80,30 @@ int main() {
         // LOGIC
         sf::Time elapsed = clock.restart();
 
-        sf::FloatRect wall_bounds = wall.getGlobalBounds();
-
-        player.animate(elapsed, wall_bounds);
-        player.gravity(elapsed, wall_bounds);
+        for (const auto &s : platforms) {
+            sf::FloatRect platform_bounds = (*s)->getGlobalBounds();
+            player.animate(elapsed, platform_bounds);
+            player.gravity(elapsed, platform_bounds);
+        }
 
         // DRAW
-        // clear the window with black color
+        // Clear the window with black color
         window.clear(sf::Color::Black);
 
-        // draw everything here...
-        window.draw(wall);
-        window.draw(circle);
-        window.draw(rectangle);
-        window.draw(triangle);
+        // Draw everything here...
+        for (const auto &s : platforms) {
+            window.draw(*s);
+        }
+
+        /*window.draw(wall1);
+        window.draw(wall2);
+        window.draw(wall3);
+        window.draw(wall4);*/
+
+        // Draw player
         window.draw(player);
 
-        // end the current frame
+        // End the current frame
         window.display();
     }
 
